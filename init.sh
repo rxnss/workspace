80eadbde703cbdfc5321a39341c3cf56eafa82e8
Weibo <weibo.zhou6fe@gmail.com>
2025-06-22 03:56:57 +0000

    ollama models

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ pip install torch
 pip install matplotlib
 # ollama 
 [ -e /usr/local/bin/ollama ] || curl -fsSL https://ollama.com/install.sh | sh 
+ollama pull qwen3:8b
+ollama pull deepseek-r1
 
 ln -sf `realpath config/screenrc` ~/.screenrc
 
